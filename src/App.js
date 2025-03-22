6e42f6b7e97ecbc37e3b16492020f95f4b834ff0
app-io-web <joaocarloslopesbarbosa@gmail.com>
2025-03-22 15:16:14 -0300

    ajuste rota

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Dashboard from "./pages/Dashboard";
@@ -5,6 +6,19 @@ import Metrics from "./pages/Metrics";
 import Login from "./pages/Login";
 
 function App() {
+  // ⛑ Redirecionador automático no client
+  useEffect(() => {
+    const pathname = window.location.pathname;
+    const hasHash = window.location.hash.startsWith("#/");
+    const isRoot = pathname === "/" || pathname.includes("index.html");
+
+    if (!hasHash && !isRoot) {
+      const search = window.location.search || "";
+      const hashPath = "#" + pathname + search;
+      window.location.replace("/" + hashPath);
+    }
+  }, []);
+
   return (
     <HashRouter>
       <Routes>
