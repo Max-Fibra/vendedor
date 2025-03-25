86277feb28ee2b366517d233de10f89e7c371b97
app-io-web <joaocarloslopesbarbosa@gmail.com>
2025-03-25 12:36:43 -0300

    INDICAÇÕES

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { HashRouter } from 'react-router-dom';
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -7,9 +9,12 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
